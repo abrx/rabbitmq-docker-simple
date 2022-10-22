22857e6e89adb1fc1c8e1fee6c3aa119a05f862a
Antoine Baroux <antoine.baroux@gmx.fr>
2022-10-22 11:10:16 +0200

    :rocket: envoi à la queue et l'exchange

--- send.js
+++ send.js
@@ -17,16 +17,22 @@ amqp.connect('amqp://localhost', function(error0, connection) {
             throw error1;
         }
         
-        // On déclare la queue utilisée
-        // Ici on utilise l'exchange par défaut qui porte le même nom de la queue, sans faire de routage
-        var queue = 'to_review';
-
         // Le message envoyé est passé en arg au script ou Hello World par défaut
+        // Pour la démo il sera envoyé plusieurs fois, directement à une queue puis en passant par un exchange
         var msg = process.argv.slice(2).join(' ') || "Hello World!";
 
-        // On envoie un message directement à une queue, sans utiliser le routage
+        
+        // Cas 1: on  utilise l'exchange par défaut qui porte le même nom de la queue cible, sans faire de routage, mais ça pert un peu d'intérêt
+        var queue = 'to_review';
         channel.sendToQueue(queue, Buffer.from(msg));
-        console.log(" [x] Sent %s", msg);
+        console.log(" [x] Sent %s directly to queue %s", msg, queue);
+      
+        
+        // Cas 2: on déclare l'exchange utilisé => le routage configuré l'enverra sur les bonnes queues
+        var exchange = 'broadcast2all';
+        channel.publish(exchange, '', Buffer.from(msg));
+        console.log(" [x] Sent %s to exchange %s", msg, exchange);        
+
     });
 
     // Clôture de la connexion et exit
