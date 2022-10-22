ee925a1bc6db95f1af0741ffa2a3ffd0c8524e92
Antoine Baroux <antoine.baroux@gmx.fr>
2022-10-22 11:10:01 +0200

    :rocket: manual ack

--- receive.js
+++ receive.js
@@ -16,13 +16,26 @@ amqp.connect('amqp://localhost', function(error0, connection) {
 
         // On déclare la queue qu'on va scruter
         var queue = 'to_review';
+
+        // On limite le nombre de messages que peut traiter chaque worker (pour paralléliser plus efficacement)
+        channel.prefetch(1);
+
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
         // On s'abonne à la queue grâce à un callback envoyé à la queue par channel.consume et on affiche les messages reçus
         channel.consume(queue, function(msg) {
+            var secs = msg.content.toString().split('.').length - 1;
+      
             console.log(" [x] Received %s", msg.content.toString());
-        }, {
-            noAck: true
+            
+            // On fait semblant de bosser avant d'envoyer l'acknowledge
+            setTimeout(function() {
+              console.log(" [x] Done");
+              channel.ack(msg);
+            }, secs * 1000);
+          }, {
+            // manual acknowledgment mode
+            noAck: false
+          });
         });
-    });
-});
+      });
